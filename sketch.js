bf981b2f6fa3fb490727b1b79bc59a6e0c43b4d5
Abhishek <AbhiKrishu7>
2021-06-04 13:28:32 +0530

    reset

--- sketch.js
+++ sketch.js
@@ -9,6 +9,14 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var cr1,cr2,cr3,cr4,track
+function preload(){
+  cr1=loadImage("images/car1.png")
+  cr2=loadImage("images/car2.png")
+  cr3=loadImage("images/car3.png")
+  cr4=loadImage("images/car4.png")
+  track=loadImage("images/track.jpg")
+}
 
 
 function setup(){
@@ -27,5 +35,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end()
   }
 }
